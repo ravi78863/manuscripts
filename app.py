4ff2a9b292bc0841ea4825a2de8ade28bb36fd62
AMMATI RAVI TEJA <ravi.22bce20257@vitapstudent.ac.in>
2025-11-07 19:06:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -109,5 +109,7 @@ def upload_file():
 
 # --- App Runner ---
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    print(f"🚀 Starting Flask on port {port}")
+    app.run(host="0.0.0.0", port=port, debug=False)
