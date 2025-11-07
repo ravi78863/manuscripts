1e079c5c705185a389e233bd347adb7fcdfe1b1a
AMMATI RAVI TEJA <ravi.22bce20257@vitapstudent.ac.in>
2025-11-07 17:40:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -102,5 +102,5 @@ def health():
     return jsonify({"status": "ok"}), 200
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
